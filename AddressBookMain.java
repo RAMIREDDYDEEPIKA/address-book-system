ef1e5abcc2c3b2d4a551bc53fb05688b9131cd58
RAMIREDDYDEEPIKA <151945678+RAMIREDDYDEEPIKA@users.noreply.github.com>
2024-11-10 23:02:49 +0530

    Deleting a specified person contacts

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -6,7 +6,7 @@ public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book!");
         Scanner sc=new Scanner(System.in);
-        Contacts[] details = new Contacts[100];  // Array to hold contacts
+        Contacts[] details = new Contacts[100];   
         int contactCount = 0;
         while (true) {
             System.out.println("\n1. Add Contact");
@@ -14,12 +14,10 @@ public class AddressBookMain {
             System.out.println("3. Delete Contact by Name");
             System.out.print("Choose an option: ");
             int option = sc.nextInt();
-            sc.nextLine();  // Consume the newline character
+            sc.nextLine();   
 
-            if (option == 1) {
-                // Add a new contact
+            if (option == 1) { 
                 System.out.println("Enter contact details:");
-
                 System.out.print("First Name: ");
                 String firstName = sc.nextLine();
                 System.out.print("Last Name: ");
@@ -36,21 +34,20 @@ public class AddressBookMain {
                 String phoneNum = sc.nextLine();
                 System.out.print("Email ID: ");
                 String emailId = sc.nextLine();
-
-                // Create new contact with the provided details
+ 
                 Contacts contact = new Contacts(firstName, lastName, address, city, state, zipNumber, phoneNum, emailId);
-
-                // Add the contact to the array
+ 
                 if (contactCount < details.length) {
                     details[contactCount] = contact;
                     contactCount++;
                     System.out.println("Contact added: " + contact);
-                } else {
+                } 
+                else {
                     System.out.println("Address book is full. Cannot add more contacts.");
                 }
 
-            } else if (option == 2) {
-                // Edit an existing contact by name
+            } 
+            else if (option == 2) {
                 System.out.print("Enter the First Name of the contact to edit: ");
                 String editFirstName = sc.nextLine();
                 System.out.print("Enter the Last Name of the contact to edit: ");
@@ -58,12 +55,9 @@ public class AddressBookMain {
 
                 boolean contactFound = false;
                 for (int i = 0; i < contactCount; i++) {
-                    if (details[i].getFirstName().equalsIgnoreCase(editFirstName) &&
-                            details[i].getLastName().equalsIgnoreCase(editLastName)) {
+                    if (details[i].getFirstName().equalsIgnoreCase(editFirstName) && details[i].getLastName().equalsIgnoreCase(editLastName)) {
                         contactFound = true;
                         System.out.println("Editing contact: " + details[i]);
-
-                        // Ask for new details and update the contact
                         System.out.print("New First Name: ");
                         details[i].setFirstName(sc.nextLine());
                         System.out.print("New Last Name: ");
@@ -89,8 +83,8 @@ public class AddressBookMain {
                 if (!contactFound) {
                     System.out.println("Contact with name " + editFirstName + " " + editLastName + " not found.");
                 }
-            } else if (option == 3) {
-                // Delete an existing contact by name
+            } 
+            else if (option == 3) { 
                 System.out.print("Enter the First Name of the contact to delete: ");
                 String deleteFirstName = sc.nextLine();
                 System.out.print("Enter the Last Name of the contact to delete: ");
@@ -98,17 +92,15 @@ public class AddressBookMain {
 
                 boolean contactFound = false;
                 for (int i = 0; i < contactCount; i++) {
-                    if (details[i].getFirstName().equalsIgnoreCase(deleteFirstName) &&
-                            details[i].getLastName().equalsIgnoreCase(deleteLastName)) {
+                    if (details[i].getFirstName().equalsIgnoreCase(deleteFirstName) && details[i].getLastName().equalsIgnoreCase(deleteLastName)) {
                         contactFound = true;
                         System.out.println("Deleting contact: " + details[i]);
-
-                        // Shift all contacts after the deleted one
+ 
                         for (int j = i; j < contactCount - 1; j++) {
                             details[j] = details[j + 1];
                         }
-                        details[contactCount - 1] = null;  // Clear the last contact
-                        contactCount--;  // Decrement contact count
+                        details[contactCount - 1] = null; 
+                        contactCount--; 
                         System.out.println("Contact deleted.");
                         break;
                     }
@@ -117,17 +109,14 @@ public class AddressBookMain {
                 if (!contactFound) {
                     System.out.println("Contact with name " + deleteFirstName + " " + deleteLastName + " not found.");
                 }
-            } else {
+            }
+            else {
                 System.out.println("Invalid option. Please try again.");
             }
-
-            // Display all contacts after each operation
             System.out.println("\nAll Contacts:");
             for (int i = 0; i < contactCount; i++) {
                 System.out.println(details[i]);
             }
-
-            // Ask if the user wants to continue
             System.out.print("\nDo you want to continue? (yes/no): ");
             String continueInput = sc.nextLine();
             if (!continueInput.equalsIgnoreCase("yes")) {
