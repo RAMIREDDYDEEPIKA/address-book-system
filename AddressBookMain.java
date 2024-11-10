39525f5e4a3d0b94e62a403689df9be4a365f752
RAMIREDDYDEEPIKA <ramireddydeepika@gmail.com>
2024-11-10 21:42:43 +0530

    Adding new contacts to Address Book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,7 +1,53 @@
 package com.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome to Address Book!");
+        Scanner sc=new Scanner(System.in);
+        Contacts[] details = new Contacts[100];  // Array to hold contacts
+        int contactCount = 0;
+        while(true){
+            System.out.println("Enter contact details:");
+            System.out.print("First Name: ");
+            String firstName = sc.nextLine();
+            System.out.print("Last Name: ");
+            String lastName = sc.nextLine();
+            System.out.print("Address: ");
+            String address = sc.nextLine();
+            System.out.print("City: ");
+            String city = sc.nextLine();
+            System.out.print("State: ");
+            String state = sc.nextLine();
+            System.out.print("Zip Code: ");
+            String zipNumber = sc.nextLine();
+            System.out.print("Phone Number: ");
+            String phoneNum = sc.nextLine();
+            System.out.print("Email ID: ");
+            String emailId = sc.nextLine();
+
+            Contacts contact = new Contacts(firstName, lastName, address, city, state, zipNumber, phoneNum, emailId);
+
+            if (contactCount < details.length) {
+                details[contactCount] = contact;
+                contactCount++;
+                System.out.println("Contact added: " + contact);
+            } else {
+                System.out.println("Address book is full. Cannot add more contacts.");
+            }
+
+            System.out.print("Do you want to add another contact? (yes/no): ");
+            String continueInput = sc.nextLine();
+            if (!continueInput.equalsIgnoreCase("yes")) {
+                break;
+            }
+        }
+
+        System.out.println("All Contacts:");
+        for (int i = 0; i < contactCount; i++) {
+            System.out.println(details[i]);
+        }
     }
 }
+
