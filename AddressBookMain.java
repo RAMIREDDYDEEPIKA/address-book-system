3109feae42ecc6bbf4ce8e1f2501a715de5dc49b
RAMIREDDYDEEPIKA <151945678+RAMIREDDYDEEPIKA@users.noreply.github.com>
2024-11-11 15:53:16 +0530

    Editing Existing contacts details

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -6,19 +6,17 @@ public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book!");
         Scanner sc=new Scanner(System.in);
-        Contacts[] details = new Contacts[100];  // Array to hold contacts
+        Contacts[] details = new Contacts[100];   
         int contactCount = 0;
         while (true) {
             System.out.println("\n1. Add Contact");
             System.out.println("2. Edit Contact by Name");
             System.out.print("Choose an option: ");
             int option = sc.nextInt();
-            sc.nextLine();  // Consume the newline character
+            sc.nextLine();  
 
-            if (option == 1) {
-                // Add a new contact
+            if (option == 1) { 
                 System.out.println("Enter contact details:");
-
                 System.out.print("First Name: ");
                 String firstName = sc.nextLine();
                 System.out.print("Last Name: ");
@@ -35,21 +33,20 @@ public class AddressBookMain {
                 String phoneNum = sc.nextLine();
                 System.out.print("Email ID: ");
                 String emailId = sc.nextLine();
-
-                // Create new contact with the provided details
+ 
                 Contacts contact = new Contacts(firstName, lastName, address, city, state, zipNumber, phoneNum, emailId);
-
-                // Add the contact to the array
+ 
                 if (contactCount < details.length) {
                     details[contactCount] = contact;
                     contactCount++;
                     System.out.println("Contact added: " + contact);
-                } else {
+                }
+                else {
                     System.out.println("Address book is full. Cannot add more contacts.");
                 }
 
-            } else if (option == 2) {
-                // Edit an existing contact by name
+            }
+            else if (option == 2) { 
                 System.out.print("Enter the First Name of the contact to edit: ");
                 String editFirstName = sc.nextLine();
                 System.out.print("Enter the Last Name of the contact to edit: ");
@@ -57,12 +54,9 @@ public class AddressBookMain {
 
                 boolean contactFound = false;
                 for (int i = 0; i < contactCount; i++) {
-                    if (details[i].getFirstName().equalsIgnoreCase(editFirstName) &&
-                            details[i].getLastName().equalsIgnoreCase(editLastName)) {
+                    if (details[i].getFirstName().equalsIgnoreCase(editFirstName) && details[i].getLastName().equalsIgnoreCase(editLastName)) {
                         contactFound = true;
                         System.out.println("Editing contact: " + details[i]);
-
-                        // Ask for new details and update the contact
                         System.out.print("New First Name: ");
                         details[i].setFirstName(sc.nextLine());
                         System.out.print("New Last Name: ");
@@ -88,17 +82,16 @@ public class AddressBookMain {
                 if (!contactFound) {
                     System.out.println("Contact with name " + editFirstName + " " + editLastName + " not found.");
                 }
-            } else {
+            } 
+            else {
                 System.out.println("Invalid option. Please try again.");
             }
-
-            // Display all contacts after each operation
+ 
             System.out.println("\nAll Contacts:");
             for (int i = 0; i < contactCount; i++) {
                 System.out.println(details[i]);
             }
-
-            // Ask if the user wants to continue
+ 
             System.out.print("\nDo you want to continue? (yes/no): ");
             String continueInput = sc.nextLine();
             if (!continueInput.equalsIgnoreCase("yes")) {
