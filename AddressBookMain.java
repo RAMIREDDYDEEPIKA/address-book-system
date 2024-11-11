277fe89f4492d32e14760f066f057c20c3e24b69
RAMIREDDYDEEPIKA <151945678+RAMIREDDYDEEPIKA@users.noreply.github.com>
2024-11-11 22:52:16 +0530

    Added Person contacts

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -2,29 +2,14 @@ package com.addressbook;
 
 import java.util.Scanner;
 
-public class AddressBookMain {
+public class AddressBookMain {  
+    
     public static void main(String[] args) {
+        
         System.out.println("Welcome to Address Book!");
-
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter contact details:");
-        System.out.print("First Name: ");
-        String firstName = sc.nextLine();
-        System.out.print("Last Name: ");
-        String lastName = sc.nextLine();
-        System.out.print("Address: ");
-        String address = sc.nextLine();
-        System.out.print("City: ");
-        String city = sc.nextLine();
-        System.out.print("State: ");
-        String state = sc.nextLine();
-        System.out.print("Zip Code: ");
-        String zipNumber = sc.nextLine();
-        System.out.print("Phone Number: ");
-        String phoneNum = sc.nextLine();
-        System.out.print("Email ID: ");
-        String emailId = sc.nextLine();
-
-        Contacts contact = new Contacts(firstName, lastName, address, city, state, zipNumber, phoneNum, emailId);
+        Contacts contact = AddContacts.AddingDetails();
+        if (contact != null) {
+            contact.toString();
+        }
     }
 }
