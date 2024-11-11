a2c7ce95368c70e118277a806a3409d433883b3d
RAMIREDDYDEEPIKA <ramireddydeepika@gmail.com>
2024-11-11 22:55:21 +0530

    Added person contacts

---
+++ AddContacts.java
@@ -0,0 +1,30 @@
+package com.addressbook;
+
+import java.util.Scanner;
+
+public class AddContacts {
+
+    public static Contacts AddingDetails()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\nEnter the first name = ");
+        String firstname = scanner.nextLine();
+        System.out.print("Enter the last name = ");
+        String lastname = scanner.nextLine();
+        System.out.print("Enter the Address = ");
+        String address = scanner.nextLine();
+        System.out.print("Enter the city = ");
+        String city = scanner.nextLine();
+        System.out.print("Enter the state = ");
+        String state = scanner.nextLine();
+        System.out.print("Enter the zip code = ");
+        String zipNumber = scanner.nextLine();
+        System.out.print("Enter the Phone number = ");
+        String phoneNum = scanner.nextLine();
+        System.out.print("Enter the Email Id = ");
+        String emailId = scanner.nextLine();
+
+        return new Contacts(firstname, lastname, address,city,state,zipNumber,phoneNum,emailId);
+    }
+}
